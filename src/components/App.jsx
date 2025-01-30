2843cd18168fd11515362133e5021db95bcd6999
Katya <katyakhomenko2105@gmail.com>
2025-01-30 22:01:07 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 import SigninPage from '../pages/SigninPage/SigninPage';
-import SignupPage from '../pages/SingupPage/SingupPage';
+import SingupPage from '../pages/SingupPage/SingupPage';
 import WelcomePage from '../pages/WelcomePage/WelcomePage';
 
 
