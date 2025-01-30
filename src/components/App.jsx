b9dc5b9d2ca54ef87f63a789b15975c35541b0b4
Katya <katyakhomenko2105@gmail.com>
2025-01-30 22:00:38 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import NotFoundPage from '../pages/NotFoundPage';
-import SigninPage from '../pages/SigninPage';
-import SignupPage from '../pages/SignupPage';
-import WelcomePage from '../pages/WelcomePage';
+import HomePage from '../pages/HomePage/HomePage';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import SigninPage from '../pages/SigninPage/SigninPage';
+import SignupPage from '../pages/SingupPage/SingupPage';
+import WelcomePage from '../pages/WelcomePage/WelcomePage';
+
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/signin" element={<SigninPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signup" element={<SingupPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
