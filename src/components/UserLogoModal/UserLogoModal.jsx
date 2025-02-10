c7658859f6cc42ee86f5b33dcf35bf9f50d63029
Katya Khomenko <katyakhomenko2105@gmail.com>
2025-02-11 01:00:28 +0200

    commit

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,82 +1,85 @@
 import { useEffect, useRef, useState } from "react";
-import { HiCog6Tooth } from "react-icons/hi2";
-import { HiOutlineLogout } from "react-icons/hi";
 import css from "./UserLogoModal.module.css";
 import UserLogoutModal from "../UserLogoutModal/UserLogoutModal";
 import SettingModal from "../SettingModal/SettingModal";
 import { useDispatch } from "react-redux";
-import { logOutModal, settingModal } from "../../redux/modal/slice";
+import spriteIcon from '../../assets/icons/sprite.svg';
 
+const UserLogoModal = ({ setIsOpenUserModal }) => {
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
 
-const UserLogoModal = () => {
   const modalRef = useRef(null);
-  const dispatch = useDispatch();
-
-  // const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
-  // const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
-
-  const handleLogoutClick = (event) => {
-    event.stopPropagation();
-    console.log("Logout button clicked!");
-    // setIsLogoutModalOpen(true);
-    dispatch(logOutModal());
-    console.log("isLogoutModalOpen:", true);
-  };
-
-  const handleSettingsClick = (event) => {
-    event.stopPropagation();
-    dispatch(settingModal());
-    // setIsSettingsModalOpen(true);
-  };
-
-  const handleClickOutside = (event) => {
-    if (modalRef.current && !modalRef.current.contains(event.target)) {
-      setIsDropdownOpen(false);
-      // setIsLogoutModalOpen(false);
-      // setIsSettingsModalOpen(false);
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Escape") {
-      setIsDropdownOpen(false);
-      // setIsLogoutModalOpen(false);
-      // setIsSettingsModalOpen(false);
-    }
-  };
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.removeEventListener("keydown", handleKeyDown);
+    const handleClickOutside = (event) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target) &&
+        !event.target.closest('.userLogoButton') &&
+        !isSettingModalOpen &&
+        !isLogoutModalOpen
+      ) {
+        setIsOpenUserModal(false);
+      }
     };
-  }, []);
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [setIsOpenUserModal, isSettingModalOpen, isLogoutModalOpen]);
 
   return (
     <>
-      {isDropdownOpen && (
-        <div className={css.dropdownMenu} ref={modalRef}>
-          <button className={css.settingsBtn} onClick={handleSettingsClick}>
-            <HiCog6Tooth className={css.settingsIcon} />
-            Settings
+      <ul className={css.dropDownMenu} ref={modalRef}>
+        <li>
+          <button
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsSettingModalOpen(true);
+            }}
+            className={css.dropDownButton}
+          >
+            <svg width="16" height="16" className={css.icon}>
+              <use href={`${spriteIcon}#icon-settings`}></use>
+            </svg>
+            Setting
           </button>
-          <button className={css.logoutBtn} onClick={handleLogoutClick}>
-            <HiOutlineLogout className={css.logoutIcon} />
+        </li>
+        <li>
+          <button
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsLogoutModalOpen(true);
+            }}
+            className={css.dropDownButton}
+          >
+            <svg width="16" height="16" className={css.icon}>
+              <use href={`${spriteIcon}#icon-logout`}></use>
+            </svg>
             Log out
           </button>
-        </div>
+        </li>
+      </ul>
+      {isLogoutModalOpen && (
+        <UserLogoutModal
+          isOpen={isLogoutModalOpen}
+          onClose={() => {
+            setIsLogoutModalOpen(false);
+          }}
+        />
       )}
-      {/* Display SettingModal if open */}
-      {/* {isSettingsModalOpen && (<SettingModal onClose={() => setIsSettingsModalOpen(false)}/>)} */}
-      <SettingModal/>
-      {/* <SettingModal/> */}
-      {/**/}
-      {/* {isLogoutModalOpen && (<UserLogoutModal onClose={() => setIsLogoutModalOpen(false)} />)} */}
-      <UserLogoutModal/>
+      {isSettingModalOpen && (
+        <SettingModal
+          isOpen={isSettingModalOpen}
+          onClose={() => {
+            setIsSettingModalOpen(false);
+          }}
+        />
+      )}
     </>
   );
 };
