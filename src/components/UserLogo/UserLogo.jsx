dd0409d48626243fe73fe3bf531ff05dd7947df0
Katya Khomenko <katyakhomenko2105@gmail.com>
2025-02-11 23:52:13 +0200

    fix header

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,27 +1,41 @@
-import { useMemo, useState } from "react";
+import { useMemo, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import UserLogoModal from "../UserLogoModal/UserLogoModal";
 import { HiOutlineChevronDown } from "react-icons/hi2";
 import css from "./UserLogo.module.css";
+import { selectUser } from "../../redux/userDataSettings/selectors";
+import { getUserInfo } from "../../redux/userDataSettings/operations";
 
-export default function UserLogo({ user = {} }) {
+export default function UserLogo() {
+  const dispatch = useDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const user = useSelector(selectUser); 
+  const [avatar, setAvatar] = useState(user?.data?.avatarUrl || null);
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setAvatar(user?.data?.avatarUrl || null);
+  }, [user?.data?.avatarUrl]);
 
   const toggleModal = () => setIsModalOpen(!isModalOpen);
   const closeModal = () => setIsModalOpen(false);
 
   const initials = useMemo(() => {
-    if (user?.name) return user.name[0].toUpperCase();
-    if (user?.email) return user.email[0].toUpperCase();
+    if (user?.data?.name) return user.data.name[0].toUpperCase();
+    if (user?.data?.email) return user.data.email[0].toUpperCase();
     return "?";
-  }, [user.name, user.email]);
+  }, [user?.data?.name, user?.data?.email]);
 
   return (
     <>
       <div className={css.userLogo} onClick={toggleModal}>
-        <span className={css.userName}>{user.name || user.email || "User"}</span>
+        <span className={css.userName}>{user?.data?.name || user?.data?.email || "User"}</span>
         <div className={css.avatarWrapper}>
-          {user?.avatar ? (
-            <img className={css.userAvatar} src={user.avatar} alt={initials} />
+          {avatar ? (
+            <img className={css.userAvatar} src={avatar} alt={initials} />
           ) : (
             <span className={css.initials}>{initials}</span>
           )}
@@ -29,7 +43,6 @@ export default function UserLogo({ user = {} }) {
         <HiOutlineChevronDown className={css.dropdownIcon} />
         {isModalOpen && <UserLogoModal closeModal={closeModal} />}
       </div>
-
     </>
   );
 }
